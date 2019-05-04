27d3621166138b31329e914fe900f2e31809bf20
debrayona <debra.chacaliaz@unmsm.edu.pe>
2019-05-04 08:04:45 -0500

    dev

--- voucher.py
+++ voucher.py
@@ -22,7 +22,28 @@ def get():
     conn =mysql.connect()
     cur = conn.cursor()
 
-    cur.callproc('transaccion_deposito', (remitente,receptor,monto,nombreT,tipo))
+    cur.callproc('InsertarVoucher', (numero_voucher,nombre_alumno,codigo_alumno))
+    cur.fetchall()
+
+    conn.commit()
+
+    return jsonify({'resultado' :'exito' })
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/buscar', methods=['GET'])
+def get():
+    numero_voucher=request.args.get('numero_voucher')
+    nombre_alumno=request.args.get('nombre_alumno')
+    codigo_alumno=request.args.get('codigo_alumno')
+    
+    print(numero_voucher)
+    conn =mysql.connect()
+    cur = conn.cursor()
+
+    alumno= cur.callproc('ByVoucher', (numero_voucher))
     cur.fetchall()
 
     conn.commit()
